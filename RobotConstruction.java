17b3d7c3316b6d80a9c16d354b2fc2c4f1605d26
Adam Jenkins <adam.jenkins69@me.com>
2026-01-28 18:54:06 +0000

    remove spoilers

--- RobotConstruction.java
+++ RobotConstruction.java
@@ -1,8 +1,4 @@
 import java.util.Arrays;
-import java.util.HashMap;
-import java.util.List;
-import java.util.Map;
-import java.util.Set;
 
 public class RobotConstruction {
     public static void main(String[] args) {
